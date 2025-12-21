9d1030fc337b6c0674e8805bb4a333dfe38bf33c
lwb <lwb2hml@gmail.com>
2025-12-22 01:59:07 +0800

    feat: enhance plugin functionality with HTTP request support

--- plugins/src/index.d.ts
+++ plugins/src/index.d.ts
@@ -2,11 +2,13 @@ declare module 'main' {
   export function filterFileName(): I32;
   export function actionAddToAlbum(): I32;
   export function actionArchive(): I32;
+  export function actionSendWebhook(): I32;
 }
 
 declare module 'extism:host' {
   interface user {
     updateAsset(ptr: PTR): I32;
     addAssetToAlbum(ptr: PTR): I32;
+    sendHttpRequest(ptr: PTR): I32;
   }
 }
